779348bf43cf765ce43d99cc8161d498c9801438
Israel Sánchez <israelclarinete@gmail.com>
2024-05-13 21:30:18 +0200

    Test con Jest

---
+++ app.js
@@ -0,0 +1,20 @@
+// One euro is:
+let oneEuroIs = {
+  JPY: 156.5, // japan yen
+  USD: 1.07, // us dollar
+  GBP: 0.87, // british pound
+};
+
+const fromEuroToDollar = (valueInEuro) => {
+  let valueInDollar = valueInEuro * oneEuroIs.USD;
+  return valueInDollar;
+};
+const fromDollarToYen = (valueInDollar) => {
+  let valueInYen = (valueInDollar / oneEuroIs.USD) * oneEuroIs.JPY;
+  return valueInYen;
+};
+const fromYenToPound = (valueInYen) => {
+  let valueInPound = (valueInYen / oneEuroIs.JPY) * oneEuroIs.GBP;
+  return valueInPound;
+};
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
---
+++ app.test.js
@@ -0,0 +1,66 @@
+const {
+  fromEuroToDollar,
+  fromDollarToYen,
+  fromYenToPound,
+} = require("./app.js");
+
+describe("Conversion from Euro to Dollars", () => {
+  test("One euro should be 1.07 dollars", () => {
+    const dollars = fromEuroToDollar(3.5);
+    const expected = 3.5 * 1.07;
+
+    expect(dollars).toBe(expected);
+  });
+
+  test("Conversion que utiliza valores numericos", () => {
+    const dollar = fromEuroToDollar(3.5);
+
+    expect(typeof dollar).toBe("number");
+  });
+
+  test("Funcion que utiliza caracteres de texto", () => {
+    const eurosToDollars = fromEuroToDollar("3.5");
+
+    expect(eurosToDollars).not.toBeNaN();
+  });
+});
+
+describe("Conversion from Dollar to Yens", () => {
+  test("One dollar should be 163.5514 yen", () => {
+    const yen = fromDollarToYen(3.5);
+    const expected = 3.5 * (1 / 1.07) * 156.5;
+
+    expect(yen).toBeCloseTo(expected);
+  });
+
+  test("Conversion que utiliza valores numericos", () => {
+    const yen = fromDollarToYen(3.5);
+
+    expect(typeof yen).toBe("number");
+  });
+
+  test("Funcion que utiliza caracteres de texto", () => {
+    const dollarsToYen = fromDollarToYen("10");
+
+    expect(dollarsToYen).not.toBeNaN();
+  });
+});
+
+describe("Conversion from Yen to Pounds", () => {
+  test("One yen should be 0.005557702194357366 pounds", () => {
+    const pound = fromYenToPound(3.5);
+    const expected = 3.5 * (1 / 156.5) * 0.87;
+
+    expect(pound).toBe(expected);
+  });
+
+  test("Conversion que utiliza valores numericos", () => {
+    const pound = fromYenToPound(3.5);
+
+    expect(typeof pound).toBe("number");
+  });
+  test("Funcion que utiliza caracteres de texto", () => {
+    const yenToPound = fromYenToPound("500");
+    expect(yenToPound).not.toBeNaN();
+  });
+});
